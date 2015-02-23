9bc3fde8e84c3c377f145d61e5dc9af230a9084d
Austin Gore <austin.gore@gmail.com>
2015-02-23 15:08:12 -0500

    Completed load_course_database.

--- Lab5/course_reader.py
+++ Lab5/course_reader.py
@@ -1,8 +1,20 @@
 __author__ = 'austin'
 
+import csv
+import sqlite3
+
 
 def load_course_database(db_name, csv_filename):
-    pass
+    conn = sqlite3.connect(db_name)
+    with conn:
+        curr = conn.cursor()
+        with open(csv_filename, 'rU') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                print(tuple(row))
+                sql_cmd = "insert into coursedata values(?,?,?,?,?,?,?)"
+                curr.execute(sql_cmd, tuple(row))
+
 
 if __name__ == '__main__':
-    load_course_database()
+    load_course_database('course1.db', 'seas-courses-5years.csv')
